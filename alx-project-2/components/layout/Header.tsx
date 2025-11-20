3eeba4c40833cb32aed188747091e56b0be86513
Grace Ghansah <gracegh@stlghana.com>
2025-11-20 08:43:51 +0000

    updated project with some components

---
+++ alx-project-2/components/layout/Header.tsx
@@ -0,0 +1,10 @@
+const Header: React.FC = () => {
+    return(
+        <>
+        
+          <h1 className="text-center h-[10rem] py-4 text-[2rem] flex items-center justify-center">SoloForge: Mastering Project Autonomy</h1>
+
+        </>
+    )
+}
+export default Header;
